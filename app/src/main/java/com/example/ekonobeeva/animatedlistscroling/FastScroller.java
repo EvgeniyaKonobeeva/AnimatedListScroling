45d8a0dbd6aac0ebdf683b2242a1ec85fa647443
evgeniya <zknbeeva@mail.ru>
2016-11-08 18:57:41 +0300

    small changes

---
+++ app/src/main/java/com/example/ekonobeeva/animatedlistscroling/FastScroller.java
@@ -0,0 +1,31 @@
+package com.example.ekonobeeva.animatedlistscroling;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by e.konobeeva on 08.11.2016.
+ */
+
+public class FastScroller extends LinearLayout {
+    public FastScroller(Context context) {
+        super(context);
+    }
+
+    public FastScroller(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FastScroller(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+//    public FastScroller(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+
+    public void init(){
+
+    }
+}
